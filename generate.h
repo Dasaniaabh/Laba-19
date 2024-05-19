259ac463a02ee12e4c06e60b9481de2e97009e7b
Renat Dasania <renatdasania8@gmail.com>
2024-05-19 23:50:17 +0300

    first commit

---
+++ generate.h
@@ -0,0 +1,12 @@
+//
+// Created by MSI on 19.05.2024.
+//
+
+#ifndef LAB_19PROJECT_GENERATE_H
+#define LAB_19PROJECT_GENERATE_H
+
+int generateMatrixFile1();
+int generateMatrixFile2();
+
+
+#endif //LAB_19PROJECT_GENERATE_H
---
+++ libs/data/func/func.h
@@ -0,0 +1,20 @@
+//
+// Created by MSI on 19.05.2024.
+//
+
+#ifndef LAB_19PROJECT_FUNC_H
+#define LAB_19PROJECT_FUNC_H
+
+int Task1();
+int Task2();
+int Task3();
+int Task4();
+void Task5(const char* filename);
+void Task6(float x, const char *filename);
+void Task7(const char *filename, int i);
+void Task8(const char *filename, int n);
+void Task9(char* filename, int n);
+void Task10(char* fileF, char* fileG);
+
+
+#endif //LAB_19PROJECT_FUNC_H
